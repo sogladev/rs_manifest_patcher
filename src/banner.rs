8427e67a833cc2753325d0cd639c4f21736947ab
Jelle Meeus <sogladev@gmail.com>
2025-02-06 13:50:12 +0100

    epoch: set manifest default, banner

--- src/banner.rs
+++ src/banner.rs
@@ -3,8 +3,9 @@ use figlet_rs::FIGfont;
 pub fn print_banner() {
     let slant_font_data = include_str!("../resources/slant.flf");
     let slant_font = FIGfont::from_content(slant_font_data).unwrap();
-    let figure = slant_font.convert("Banner");
+    let figure = slant_font.convert("Project Epoch");
     print!("{}", figure.unwrap());
+    println!("unofficial patch download utility - Sogladev");
     println!("Bugs or issues: https://github.com/sogladev/rs_manifest_patcher/");
     println!("\n{}", "-".repeat(100));
 }
